6090bbbb748e98bdde82ef3e75adc679d154b5dc
Qaisar Mateen <qaisarmateen313@gmail.com>
2023-07-05 17:35:33 +0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -14,3 +14,43 @@ char** create_log(char** l, int s, char comd[]);
 void peek_log(char** l, int s);
 void Q_shell_op(char** logs, int s, char* comd);
 
+int main(int argc, char* argv[])
+{
+    system("clear");
+    printf("\n       --<><><><><><><><><><><><><( Q-SHELL )><><><><><><><><><><><><>--\n");
+    char comd[50], **logs;
+    int comd_no = 0;
+
+    while(strcmp(comd, "exit") && strcmp(comd, "EXIT"))
+    {
+        printf("\nEnter command: ");
+        scanf("\n%[^\n]%*c", comd); 
+
+        comd[strlen(comd)] = '\0';
+        
+        if(strcmp(comd, "exit") != 0 && strcmp(comd, "EXIT") != 0)
+        {
+            Q_shell_op(logs, comd_no, comd);
+            logs = create_log(logs, comd_no++, comd);
+
+            int p_no = strocc(comd, '|', NULL);
+
+            if(!strcmp(comd, "history") || !strcmp(comd, "HISTORY"))
+                peek_log(logs, comd_no);
+
+            else if(p_no > 0)
+                pipe_comd(comd, p_no);
+
+            else if(strocc(comd, '>', NULL) > 0 || strocc(comd, '<', NULL) > 0)
+                redirect_comd(comd, NULL);
+
+            else
+                normal_comd(comd); 
+        }
+    }
+
+    printf("\n\t\t\t   Terminating Q-Shell...\n\n");
+    sleep(1.2);
+    system("clear");
+    return 0;
+}
