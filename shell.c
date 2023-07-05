7b340f4dfcd142f7390318d1e2e0f642ec252598
Qaisar Mateen <qaisarmateen313@gmail.com>
2023-07-05 17:42:00 +0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -108,3 +108,161 @@ void normal_comd(char comd[])
         exit(0);
     }
 }//-------------------------------------------
+
+void redirect_comd(char comd[], int *pipe_fd)
+{
+    if(strocc(comd, '<', NULL) || strocc(comd, '>', NULL)) //checking if redirection exists
+    {
+        int comd1_size = 0, i = 1, flag = 0, arg;
+        for(comd1_size = 0; comd1_size < strlen(comd); comd1_size++) //calculating command size 
+        {
+            if(comd[comd1_size] == '<' || comd[comd1_size] == '>')
+                break;
+        }
+        char comd_r[comd1_size], op = comd[comd1_size], *file = comd;
+        strncpy(comd_r, comd, comd1_size);
+        comd_r[comd1_size-1] = '\0';
+        strtok_r(comd, &op, &file);
+        file++;
+
+        arg = strocc(comd_r, ' ', &flag);
+        char* rest = comd_r, args[arg+1][15];
+        strtok_r(comd_r, " ", &rest);
+        strncpy(args[0], comd_r, 15);
+        args[0][14] = '\0';    
+        printf("\ncommand: %s ",  args[0]);
+        arg > 0? printf("Arguments: %s  Total-Arguments: %d\n", rest, arg):printf("\n");
+        printf("File: '%s' operator: '%c' cmd: '%s'\n\n", file, op, comd_r);
+            
+        while(i < arg+1)
+        {   
+            strcpy(comd_r, rest);
+            strtok_r(comd_r, " ", &rest);
+            if(strcmp(comd_r, "&") == 0)
+            flag = 1; //concurrent execution flag
+
+            else
+            {
+                strncpy(args[i], comd_r, 15);
+                args[i][14] = '\0';
+                i++;
+            }   
+        }
+    
+        if(fork()) //parrent process
+            wait(NULL);
+
+        else {
+            int fd; 
+            
+            if(op == '>') {
+                fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                if(fd == -1){
+                    perror("Open Error Write");
+                    exit(1);
+                }
+                dup2(fd, 1); //stdout
+                if(pipe_fd)
+                    dup2(*pipe_fd, 0); //stdin
+            }
+    
+            if(op == '<') {
+                fd = open(file, O_RDONLY);
+                if(fd == -1){
+                    perror("Open Error Read");
+                    exit(1);
+                }
+                dup2(fd, 0); //stdin
+                if(pipe_fd)
+                    dup2(*pipe_fd, 1); //stdout
+            }
+            char* argum[arg+2];
+            for(i = 0; i < arg + 1; i++)
+                argum[i] = args[i];
+            argum[arg+1] = NULL;
+
+            execvp(argum[0], argum);
+            printf("Error: Unknown Command!!\n");
+            exit(0);
+        }
+    }
+}//-------------------------------------------
+
+void redirect_comd(char comd[], int *pipe_fd)
+{
+    if(strocc(comd, '<', NULL) || strocc(comd, '>', NULL)) //checking if redirection exists
+    {
+        int comd1_size = 0, i = 1, flag = 0, arg;
+        for(comd1_size = 0; comd1_size < strlen(comd); comd1_size++) //calculating command size 
+        {
+            if(comd[comd1_size] == '<' || comd[comd1_size] == '>')
+                break;
+        }
+        char comd_r[comd1_size], op = comd[comd1_size], *file = comd;
+        strncpy(comd_r, comd, comd1_size);
+        comd_r[comd1_size-1] = '\0';
+        strtok_r(comd, &op, &file);
+        file++;
+
+        arg = strocc(comd_r, ' ', &flag);
+        char* rest = comd_r, args[arg+1][15];
+        strtok_r(comd_r, " ", &rest);
+        strncpy(args[0], comd_r, 15);
+        args[0][14] = '\0';    
+        printf("\ncommand: %s ",  args[0]);
+        arg > 0? printf("Arguments: %s  Total-Arguments: %d\n", rest, arg):printf("\n");
+        printf("File: '%s' operator: '%c' cmd: '%s'\n\n", file, op, comd_r);
+            
+        while(i < arg+1)
+        {   
+            strcpy(comd_r, rest);
+            strtok_r(comd_r, " ", &rest);
+            if(strcmp(comd_r, "&") == 0)
+            flag = 1; //concurrent execution flag
+
+            else
+            {
+                strncpy(args[i], comd_r, 15);
+                args[i][14] = '\0';
+                i++;
+            }   
+        }
+    
+        if(fork()) //parrent process
+            wait(NULL);
+
+        else {
+            int fd; 
+            
+            if(op == '>') {
+                fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                if(fd == -1){
+                    perror("Open Error Write");
+                    exit(1);
+                }
+                dup2(fd, 1); //stdout
+                if(pipe_fd)
+                    dup2(*pipe_fd, 0); //stdin
+            }
+    
+            if(op == '<') {
+                fd = open(file, O_RDONLY);
+                if(fd == -1){
+                    perror("Open Error Read");
+                    exit(1);
+                }
+                dup2(fd, 0); //stdin
+                if(pipe_fd)
+                    dup2(*pipe_fd, 1); //stdout
+            }
+            char* argum[arg+2];
+            for(i = 0; i < arg + 1; i++)
+                argum[i] = args[i];
+            argum[arg+1] = NULL;
+
+            execvp(argum[0], argum);
+            printf("Error: Unknown Command!!\n");
+            exit(0);
+        }
+    }
+}//-------------------------------------------
