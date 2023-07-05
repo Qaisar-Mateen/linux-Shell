22372ef2ef435a3fc65a2f526d422a9968f1f4d9
Qaisar Mateen <qaisarmateen313@gmail.com>
2023-07-05 17:34:24 +0500

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<fcntl.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+void normal_comd(char comd[]);
+void pipe_comd(char comd[], int p_no);
+void redirect_comd(char comd[], int* pipe_fd);
+int strocc(char str[], char ch, int* f);
+char** create_log(char** l, int s, char comd[]);
+void peek_log(char** l, int s);
+void Q_shell_op(char** logs, int s, char* comd);
+
